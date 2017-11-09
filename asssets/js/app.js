3b6ade58b0ad4164bc97a9bb7afea3ab99ee47a3
camilabelen <ccornejoss@gmail.com>
2017-11-09 13:58:56 -0300

    version 0.0.3 con algunas fallas

--- asssets/js/app.js
+++ asssets/js/app.js
@@ -27,19 +27,61 @@ function saveList(){
     var listName = document.createTextNode(addList);
     var nameContainer = document.createElement("p");
     var link = document.createElement("a");
-    var addCard = document.createTextNode("Añadir tarea")
+    var textCard = document.createTextNode("Añadir tarea")
     /*agregando nodo texto contenido de textarea, 
     p contenedor del nodo texto y div contenedor de p*/
     nameContainer.appendChild(listName);
     lists.appendChild(nameContainer);
     newList.appendChild(lists);
-    /*agregando texto a la etiqueta 'a'*/
-    link.appendChild(addCard);
-    /*agregando etiqueta 'a' a su contenedor(mismo 'div' de p)*/
+    //agregando texto a la etiqueta 'a'
+    link.appendChild(textCard);
+    //agregando etiqueta 'a' a su contenedor(mismo 'div' de p)
     lists.appendChild(link);
-    /*estilos de los elementos agregados*/
+    //estilos de los elementos agregados
    	document.getElementById("newBoard").style.display ="inline-block";
-   	document.getElementById("newList").style.display ="inline-block"
-   	lists.style.display ="inline-block"
-	lists.style.margin ="35px 10px 20px 10px"
+   	document.getElementById("newList").style.display ="inline-block";
+    
+   	lists.style.display ="inline-block";
+	lists.style.margin ="35px 10px 20px 10px";
+    lists.setAttribute("id", "divForm");
+	//agregando id a etiqueta 'a'
+	link.setAttribute("id","addCard");
+	//evento a etiqueta "a"
+	var addCard = document.getElementById("addCard");
+	addCard.addEventListener("click", addCards);
 }
+
+//función para agregar tarjetas al tableros
+function addCards(){
+	addCard.style.display = "none";//ocultando 'a'
+	var container = document.getElementById("newList");
+    //variable contenedora del formulario
+    var div = document.getElementById("divForm");
+	//creando elementos que "reemplazan" a la etiqueta a (un formulario con textarea y button)
+	var form = document.createElement("form");
+	var textarea = document.createElement("textarea");
+	var cardButton = document.createElement("button");
+    //agregando elementos
+    cardButton.innerHTML = "Agregar";
+	form.appendChild(textarea);
+	form.appendChild(cardButton);
+	divForm.appendChild(form);
+
+    //estilos al botón
+    cardButton.style.display ="block";
+    cardButton.style.border ="none";
+    cardButton.style.padding = "10px";
+    cardButton.style.borderRadius = "5px";
+    cardButton.style.fontWeight = "600";
+    cardButton.style.fontSize = "1em";
+    cardButton.style.marginLeft = "10px";
+    cardButton.style.backgroundColor ="#3AD053";
+    cardButton.style.color ="#FFF";
+    //estilos textarea
+    textarea.style.marginLeft ="10px";
+    textarea.style.width = "250px";
+    textarea.style.height = "80px";
+    textarea.style.float = "left";
+}
+
+
